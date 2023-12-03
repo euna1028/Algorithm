a197b3c3615ad0090c287320024d5658e99f3fb2
Neogool <124523188+euna1028@users.noreply.github.com>
2023-12-03 18:25:18 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String code1) {
+char[] code = code1.toCharArray();
+		String ret = "";
+		int mode = 0;
+		for (int i = 0; i < code1.length(); i++) {
+            if (mode == 0) {
+				if (code[i] == '1') {
+					mode = 1;
+				}else if (code[i] != '1' && i % 2 == 0) {
+					 ret = ret.concat(String.valueOf(code[i]));
+				}
+			}else if (mode == 1) {
+				if (code[i] == '1') {
+					mode = 0;
+					
+				}else if (code[i] != '1' && i % 2 != 0) {
+					 ret = ret.concat(String.valueOf(code[i]));
+				}
+			}
+		}
+
+         return ret.equals("")? "EMPTY" : ret;
+    }
+}
